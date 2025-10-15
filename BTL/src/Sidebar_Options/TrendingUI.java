608b058bf97ee1cfe980c8592bde82fd205f1ed4
minhhai09102005-svg <minhhai09102005@gmail.com>
2025-10-15 18:30:43 +0700

    Add files via upload

---
+++ BTL/src/Sidebar_Options/TrendingUI.java
@@ -0,0 +1,19 @@
+package Sidebar_Options;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+
+public class TrendingUI extends StackPane {
+    public TrendingUI() {
+        setStyle("-fx-background-color:#010101;");
+        setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE); // cho phép fill
+        getChildren().add(new Label("Trending Screen"));
+    }
+}
